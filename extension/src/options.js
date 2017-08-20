74846dfee19fd32f1e9306063ed0ca591bea62e3
Bradley Hamm <bradjh83@gmail.com>
2017-08-20 16:49:55 -0700

    Options updates

--- extension/src/options.js
+++ extension/src/options.js
@@ -1,17 +1,26 @@
-(function () {
+/* globals chrome */
+/* jshint esversion: 6 */
 
-    var inputs = {
-        url: document.querySelector('#url')
+(function () {
+    'use strict';
+
+    let inputs = {
+        url: document.querySelector('#url'),
+        poll_frequency: document.querySelector('#poll_frequency')
     };
 
-    chrome.storage.sync.get(function (o) {
+    function save (e) {
+        let o = {};
+        o[e.target.id] = e.target.value;
+        chrome.storage.sync.set(o);
+    }
+
+    chrome.storage.sync.get(o => {
         inputs.url.value = o.url || '';
+        inputs.poll_frequency.value = o.poll_frequency || '';
     });
 
-    inputs.url.addEventListener('keyup', function (e) {
-        chrome.storage.sync.set({
-            url: e.target.value
-        });
-    });
+    inputs.url.addEventListener('keyup', save);
+    inputs.poll_frequency.addEventListener('change', save);
 
 })();
